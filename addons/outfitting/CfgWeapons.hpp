f72d063ab7751a83f9dabca04b1885ff646aa64d
Brett Mayson <brett@mayson.io>
2026-04-24 23:14:48 -0600

    hvi classname

--- addons/outfitting/CfgWeapons.hpp
+++ addons/outfitting/CfgWeapons.hpp
@@ -1,6 +1,6 @@
 class CfgWeapons {
     class H_PASGT_basic_black_F;
-    class ITEM(helmet): H_PASGT_basic_black_F {
+    class ITEM(hvi_helmet): H_PASGT_basic_black_F {
         scope = 2;
         displayName = "HVI Helmet";
         author = "Synixe Contractors";
@@ -8,7 +8,7 @@ class CfgWeapons {
     };
 
     class V_CarrierRigKBT_01_Black_F;
-    class ITEM(vest): V_CarrierRigKBT_01_Black_F {
+    class ITEM(hvi_vest): V_CarrierRigKBT_01_Black_F {
         scope = 2;
         displayName = "HVI Vest";
         author = "Synixe Contractors";
